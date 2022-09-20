7fd0db259ad3b17aac8660c6b5e16e2d7b34680e
YoseobKim <ykim9611@gmail.com>
2022-09-19 22:13:07 -0700

    Removed console log

--- client/reach_your_audience/script.js
+++ client/reach_your_audience/script.js
@@ -2,7 +2,6 @@ const navigationItems = document.querySelectorAll(".nav-items");
 
 navigationItems.forEach(function (item) {
   var handleHover = function () {
-    console.log("hi");
     navigationItems[0].firstElementChild.classList.toggle("current");
   };
 
