667ce217f1b76ee0c1af47b26c0c87fd440060a6
YoseobKim <ykim9611@gmail.com>
2022-09-19 22:12:39 -0700

    Added "What's New" route

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const port = 3001;
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "client/reach_your_audience")));
 app.use(express.static(path.join(__dirname, "client/contact_us")));
+app.use(express.static(path.join(__dirname, "client/reach_your_audience")));
+app.use(express.static(path.join(__dirname, "client/whats_new")));
 
 var publicPath = path.join(__dirname, "client");
 
@@ -18,6 +19,9 @@ app.get("/contact-us", function (req, res) {
 });
 app.get("/reach-your-audience", function (req, res) {
   res.sendFile(publicPath + "/reach_your_audience/index.html");
+});
+app.get("/whats-new", function (req, res) {
+  res.sendFile(publicPath + "/whats_new/index.html");
 });
 
 app.listen(port, () => {
